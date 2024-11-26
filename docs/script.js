543feaaea28d490ef548f3ea801f660c48539687
TalDoJay <jp.rodrigues.madara@gmail.com>
2024-11-26 16:03:46 -0300

    adcionando novas telas

--- docs/script.js
+++ docs/script.js
@@ -5,18 +5,15 @@ const sFuncao = document.querySelector('#m-funcao')
 const sSalario = document.querySelector('#m-salario')
 const btnSalvar = document.querySelector('#btnSalvar')
 
-let itens = []
+let itens
 let id
 
 function openModal(edit = false, index = 0) {
   modal.classList.add('active')
-  modal.style.opacity = 0
-  setTimeout(() => modal.style.opacity = 1, 100)
 
   modal.onclick = e => {
     if (e.target.className.indexOf('modal-container') !== -1) {
       modal.classList.remove('active')
-      modal.style.opacity = 0
     }
   }
 
@@ -30,9 +27,11 @@ function openModal(edit = false, index = 0) {
     sFuncao.value = ''
     sSalario.value = ''
   }
+  
 }
 
 function editItem(index) {
+
   openModal(true, index)
 }
 
@@ -40,7 +39,6 @@ function deleteItem(index) {
   itens.splice(index, 1)
   setItensBD()
   loadItens()
-  showToast('Item excluído com sucesso!')
 }
 
 function insertItem(item, index) {
@@ -51,36 +49,36 @@ function insertItem(item, index) {
     <td>${item.funcao}</td>
     <td>R$ ${item.salario}</td>
     <td class="acao">
-      <button class="edit-btn" onclick="editItem(${index})"><i class='bx bx-edit'></i> Editar</button>
+      <button onclick="editItem(${index})"><i class='bx bx-edit' ></i></button>
     </td>
     <td class="acao">
-      <button class="delete-btn" onclick="deleteItem(${index})"><i class='bx bx-trash'></i> Excluir</button>
+      <button onclick="deleteItem(${index})"><i class='bx bx-trash'></i></button>
     </td>
   `
   tbody.appendChild(tr)
 }
 
 btnSalvar.onclick = e => {
-  e.preventDefault()
-
+  
   if (sNome.value == '' || sFuncao.value == '' || sSalario.value == '') {
-    alert('Preencha todos os campos antes de salvar!')
     return
   }
+
+  e.preventDefault();
 
   if (id !== undefined) {
     itens[id].nome = sNome.value
     itens[id].funcao = sFuncao.value
     itens[id].salario = sSalario.value
   } else {
-    itens.push({ 'nome': sNome.value, 'funcao': sFuncao.value, 'salario': sSalario.value })
+    itens.push({'nome': sNome.value, 'funcao': sFuncao.value, 'salario': sSalario.value})
   }
 
   setItensBD()
+
   modal.classList.remove('active')
   loadItens()
   id = undefined
-  showToast('Item salvo com sucesso!')
 }
 
 function loadItens() {
@@ -89,18 +87,7 @@ function loadItens() {
   itens.forEach((item, index) => {
     insertItem(item, index)
   })
-}
 
-function showToast(message) {
-  const toast = document.createElement('div')
-  toast.classList.add('toast')
-  toast.innerText = message
-  document.body.appendChild(toast)
-
-  setTimeout(() => {
-    toast.style.opacity = 0
-    setTimeout(() => toast.remove(), 300)
-  }, 2000)
 }
 
 const getItensBD = () => JSON.parse(localStorage.getItem('dbfunc')) ?? []
